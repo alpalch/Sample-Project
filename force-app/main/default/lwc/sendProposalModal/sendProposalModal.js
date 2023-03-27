1648e4c34a8465a086ad0de67b69aa3c743ccb7f
Valeriy Palchenko <alpalch@gmail.com>
2023-03-27 15:39:35 +0300

    Refactor sendProposalModal

--- force-app/main/default/lwc/sendProposalModal/sendProposalModal.js
+++ force-app/main/default/lwc/sendProposalModal/sendProposalModal.js
@@ -2,60 +2,66 @@
  * @description       : 
  * @author            : @ValeriyPalchenko
  * @group             : 
- * @last modified on  : 24-03-2023
+ * @last modified on  : 27-03-2023
  * @last modified by  : @ValeriyPalchenko
 **/
-import { api, track, wire } from 'lwc';
+import { api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import LightningModal from 'lightning/modal';
 import getContentDocumentId from '@salesforce/apex/ManageProposalsController.getContentDocumentId';
 import sendEmailWithAttachment from '@salesforce/apex/ManageProposalsController.sendEmailWithAttachment';
-import closePreview from '@salesforce/apex/ManageProposalsController.closePreview';
-import ContactMobile from '@salesforce/schema/Case.ContactMobile';
+import deleteProposal from '@salesforce/apex/ManageProposalsController.deleteProposal';
 
-export default class PreviewModal extends LightningModal {
+const ERROR_TOAST_MESSAGE = 'Something went wrong. Ask your administrator to check logs.';
+const ERROR_TOAST_TITLE = 'Error';
+const ERROR_TOAST_VARIANT = 'error';
+const SUCCESS_SENDED_TOAST_MESSAGE = 'Email with proposal was sended';
+const SUCCESS_TOAST_TITLE = 'Success';
+const SUCCESS_TOAST_VARIANT = 'success';
+
+export default class sendProposalModal extends LightningModal {
     @api content;
-    @track result;
-    @track errors;
-    @track link;
-
-    getContentDocumentId(){
-        getContentDocumentId({ proposalId: this.content })
-            .then(result => {
-                if(result){
-                    this.result = result;
-                    this.link = '/sfc/servlet.shepherd/document/download/' + result;
-                    console.log('Success');
-                }
-            })
-            .catch(error => {
-                console.log('Error getting ContentDocumentId');
-                console.log('Error: ', error);
-            })
-    }
+    result;
+    error;
+    link;
 
     connectedCallback() {
         //Apex imperative call to avoid DML Exception
         this.getContentDocumentId();
     }
 
-    handleSendClick(){
+    getContentDocumentId() {
+        getContentDocumentId({ proposalId: this.content })
+            .then(result => {
+                if(result){
+                    this.result = result;
+                    this.link = '/sfc/servlet.shepherd/document/download/' + result;
+                }
+            })
+            .catch(error => {
+                console.log('Error: ', error);
+                this.showErrorToast();
+            })
+    }
+
+    handleSendProposal() {
         sendEmailWithAttachment( {contentDocumentId: this.result, proposalId: this.content} )
             .then(result => {
                 if(result){
-                    console.log('Sended');
+                    this.showSuccessToast(SUCCESS_SENDED_TOAST_MESSAGE);
                 }
             })
             .catch(error => {
                 if(error){
-                    console.log('Error while sending email');
-                    console.log(error);
+                    console.log('Error while sending email: ' + error);
+                    this.showErrorToast();
                 }
             })
         this.close('Sended and closed');
     }
 
-    handleCancelClick(){
-        closePreview( {contentDocumentId: this.result} )
+    handleCancel() {
+        deleteProposal( {contentDocumentId: this.result} )
             .then(result => {
                 if(result){
                     console.log('Canceled');
@@ -68,5 +74,23 @@ export default class PreviewModal extends LightningModal {
                 }
             })
         this.close('Deleted and closed');
+    }
+
+    showErrorToast() {
+        const event = new ShowToastEvent({
+            title: ERROR_TOAST_TITLE,
+            message: ERROR_TOAST_MESSAGE,
+            variant: ERROR_TOAST_VARIANT,
+        });
+        this.dispatchEvent(event);
+    }
+
+    showSuccessToast(message) {
+        const event = new ShowToastEvent({
+            title: SUCCESS_TOAST_TITLE,
+            message: message,
+            variant: SUCCESS_TOAST_VARIANT,
+        });
+        this.dispatchEvent(event);
     }
 }
